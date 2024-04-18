57533c90911007abd165dc43f85146454ae4574a
rajlathiya0 <rajlathiya1912@gmail.com>
2024-04-18 15:31:52 +0530

    commit

---
+++ lib/shared/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static String appId = "1:422344311338:web:d32596f31e74acb6533953";
+  static String apiKey = "AIzaSyDRHeWYEMku8SATbnyX1BsgFOV3KXLqGZo";
+  static String messagingSenderId = "422344311338";
+  static String projectId = "chatappflutter-8b585";
+  final primaryColor = const Color(0xFFee7b64);
+}
